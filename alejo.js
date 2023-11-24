d8c1b0fcb625152e771755b5d5dd6e8479a7e903
AlejoPaglino <136344335+AlejoPaglino@users.noreply.github.com>
2023-11-24 00:15:54 -0300

    Add files via upload

---
+++ alejo.js
@@ -0,0 +1,23 @@
+
+    <div class="conteiner hay 3">
+        <div class="card-body">
+            <h1 class="card-title"></h1>
+            <h1>A continuacion les dejo un video para ver como se prepara un mates</h1>
+            <a class="card-link" href="a"><iframe width="560" height="315" src="https://www.youtube.com/embed/vwrmTSMmcaI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> </a>
+            <a class="card-link" href="a"><iframe width="560" height="315" src="https://youtu.be/V2oz8H4c9Fc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></a>
+            <p class=""></p>
+        </div>
+        <div>
+        
+<script>
+    var elementosP = document.getElementsByTagName("p");
+    var segundo = document.getElementsByTagName ("segundo");
+    var elemento = document.createElement("h1");
+    var contenido = document.createTextNode("Este es el mate que deberia quedar")
+    elemento.appendChild(contenido);
+    elemento.setAttribute("align", "center");
+    document.getElementById("subtitulo").appendChild(elemento);
+</script>
+
+        </div>
+    </div>
